c53c802a7343a6d957969ae380454bc664963847
DonPangPang <powertle@outlook.com>
2021-12-15 20:49:16 +0800

    添加项目文件。

---
+++ src/Pang.Common.AutoMapper/Extensions/AutoMapperExtension.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Pang.Common.AutoMapper.Extensions;
+
+public static class AutoMapperExtension
+{
+    /// <summary>
+    /// </summary>
+    public static IMapper Mapper { get; set; } = null!;
+
+    /// <summary>
+    /// 配置AutoMapper中间件
+    /// </summary>
+    /// <param name="mapper"> </param>
+    public static void Configure(IMapper mapper)
+    {
+        Mapper = mapper;
+    }
+
+    /// <summary>
+    /// 添加AutoMapper
+    /// <para> 在实体类以及模型中使用Attribute: [AutoMap(typeof(Model))] </para>
+    /// <example> [AutoMap(typeof(UserDto))] class User{} </example>
+    /// </summary>
+    /// <param name="service"> </param>
+    /// <returns> </returns>
+    public static IServiceCollection AddAutoMapper(this IServiceCollection service)
+    {
+        service.AddAutoMapper(config =>
+        {
+            config.AddMaps(AppDomain.CurrentDomain.GetAssemblies());
+        });
+        return service;
+    }
+
+    /// <summary>
+    /// 映射到对象
+    /// </summary>
+    /// <typeparam name="T"> </typeparam>
+    /// <param name="obj"> </param>
+    /// <returns> </returns>
+    public static T MapTo<T>(this object obj)
+    {
+        return Mapper.Map<T>(obj);
+    }
+
+    /// <summary>
+    /// 映射到对象
+    /// </summary>
+    /// <typeparam name="T"> </typeparam>
+    /// <param name="obj"> </param>
+    /// <returns> </returns>
+    public static IEnumerable<T> MapTo<T>(this IEnumerable<object> obj)
+    {
+        return Mapper.Map<IEnumerable<T>>(obj);
+    }
+}
---
+++ src/Pang.Common.Utils/StringUtils/StringExtensions.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace Pang.Common.Utils.StringUtils;
+
+/// <summary>
+/// 字符串扩展类
+/// </summary>
+public static class StringExtensions
+{
+    /// <summary>
+    /// 转Int16
+    /// </summary>
+    /// <param name="str"> </param>
+    /// <returns> </returns>
+    public static short ToInt16(this string str)
+    {
+        if (string.IsNullOrEmpty(str) ||
+            string.IsNullOrWhiteSpace(str))
+        {
+            return 0;
+        }
+
+        Int16.TryParse(str, out var result);
+
+        return result;
+    }
+
+    /// <summary>
+    /// 转Short
+    /// </summary>
+    /// <param name="str"> </param>
+    /// <returns> </returns>
+    public static short ToIntShort(this string str)
+    {
+        if (string.IsNullOrEmpty(str) ||
+            string.IsNullOrWhiteSpace(str))
+        {
+            return 0;
+        }
+
+        Int16.TryParse(str, out var result);
+
+        return result;
+    }
+
+    /// <summary>
+    /// 转Int
+    /// </summary>
+    /// <param name="str"> </param>
+    /// <returns> </returns>
+    public static int ToInt(this string str)
+    {
+        if (string.IsNullOrEmpty(str) ||
+            string.IsNullOrWhiteSpace(str))
+        {
+            return 0;
+        }
+
+        Int32.TryParse(str, out var result);
+
+        return result;
+    }
+
+    /// <summary>
+    /// 转Int32
+    /// </summary>
+    /// <param name="str"> </param>
+    /// <returns> </returns>
+    public static int ToInt32(this string str)
+    {
+        if (string.IsNullOrEmpty(str) ||
+            string.IsNullOrWhiteSpace(str))
+        {
+            return 0;
+        }
+
+        Int32.TryParse(str, out var result);
+
+        return result;
+    }
+
+    /// <summary>
+    /// 转Long
+    /// </summary>
+    /// <param name="str"> </param>
+    /// <returns> </returns>
+    public static long ToIntLong(this string str)
+    {
+        if (string.IsNullOrEmpty(str) ||
+            string.IsNullOrWhiteSpace(str))
+        {
+            return 0;
+        }
+
+        Int64.TryParse(str, out var result);
+
+        return result;
+    }
+
+    /// <summary>
+    /// 转Int64
+    /// </summary>
+    /// <param name="str"> </param>
+    /// <returns> </returns>
+    public static long ToInt64(this string str)
+    {
+        if (string.IsNullOrEmpty(str) ||
+            string.IsNullOrWhiteSpace(str))
+        {
+            return 0;
+        }
+
+        Int64.TryParse(str, out var result);
+
+        return result;
+    }
+}
